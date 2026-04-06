968b336486a14f12964220706db89e969021bc43
Iftu Chala <iftuchala35@gmail.com>
2026-04-06 07:34:50 +0300

    Implement lengthOfLastWord method in Solution class

---
+++ length of last word.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i = len(s) - 1
+
+        while i >= 0 and s[i] == " ":
+            i =i - 1
+
+        length = 0
+
+        while i >= 0 and s[i] != " ":
+            length += 1
+            i =i- 1
+
+        return length
